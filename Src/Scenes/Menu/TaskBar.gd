a48568bf588db5bde6a633ceb4cab8b6cce840b6
lookatthesea <shane.atkinson@lookatthesea.com>
2018-04-09 21:13:41 -0400

    Added physics for single button.

--- Src/Scenes/Menu/TaskBar.gd
+++ Src/Scenes/Menu/TaskBar.gd
@@ -1,8 +1,32 @@
 extends Node2D
 
+var Vs = [-2, -6, -8] # velocities for each "bounce"
+var V
+var a
+var bounce
+var origin
 var actor
+func setup(act):
+	actor = act
+	a = 0.5
+	bounce = 2
+	V = Vs[bounce]
+	origin = actor.get_position().y
+func step(c):
+	c.y += V
+	if c.y < origin: # on the rise
+		V += a
+		return c
+	if bounce > 0: # bounce off the "ground"
+		bounce -= 1
+		V = Vs[bounce]
+		return c
+	# stop bouncing
+	self.set_physics_process(false)
+	return c
+
 func clicked(btn):
-	actor = btn
+	setup(btn)
 	self.set_physics_process(true)
 
 func _ready():
@@ -11,6 +35,4 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	if true:
-		self.set_physics_process(false)
-	pass
+	actor.set_position(step(actor.get_position()))
