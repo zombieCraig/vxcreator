a257cc5348ace6c1d8839065d07e8b012214b3b7
lookatthesea <shane.atkinson@lookatthesea.com>
2018-04-14 14:24:27 -0400

    Added mouseover hide/show functionality.

--- Src/Scenes/Menu/TaskBar.gd
+++ Src/Scenes/Menu/TaskBar.gd
@@ -1,14 +1,28 @@
 extends Control
 
-func show():
-	pass
-
+var timer = 0
 func hide():
-	$Icons.position.y += rect_size.y
-	connect("mouse_entered", self, "start_hover")
+	$Icons.position.y = 81
+	timer = 0
+	set_process(true)
 
-func start_hover():
-	print('entered')
+func show():
+	$Icons.position.y = 0
+	set_process(false)
 
 func _ready():
-	pass
+	hide()
+
+func _process(delta):
+	var port = get_viewport()
+	var h = port.get_visible_rect().size.y
+	var y = port.get_mouse_position().y
+	
+	if h - y <= 81:
+		timer += delta
+	else:
+		timer = 0
+		$Icons.position.y = 81
+	if timer >= 1:
+		timer = 1
+		$Icons.position.y = 0
