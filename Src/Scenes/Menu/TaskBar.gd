d29598326cc65c402d901f2089c506d3b3bc04f8
lookatthesea <shane.atkinson@lookatthesea.com>
2018-04-15 09:06:11 -0400

    Another try at the TaskBar

--- Src/Scenes/Menu/TaskBar.gd
+++ Src/Scenes/Menu/TaskBar.gd
@@ -1,32 +1,39 @@
 extends Control
 
-var timer = 0
-func hide():
-	$Icons.position.y = 81
-	timer = 0
+enum state { ENTERED, EXITED }
+var last_state = EXITED
+func _process(delta):
+	var h = get_viewport().get_visible_rect().size.y
+	var y = get_viewport().get_mouse_position().y
+	if h - y <= 81:
+		if last_state == EXITED:
+			$DownTimer.stop()
+			$UpTimer.start()
+			last_state = ENTERED
+	else:
+		if last_state == ENTERED:
+			$UpTimer.stop()
+			$DownTimer.start()
+			last_state = EXITED
+
+var slide_v = 0
+func slide(dir):
+	slide_v = dir * 900
 	set_physics_process(true)
 
-func show():
-	$Icons.position.y = 0
-	set_physics_process(false)
+func _physics_process(delta):
+	margin_top += slide_v * delta
+	margin_bottom += slide_v * delta
+	if slide_v < 0 and (margin_top <= -81 or margin_bottom <= 0):
+		margin_top = -81; margin_bottom = 0
+		slide_v = 0
+		set_physics_process(false)
+	elif slide_v > 0 and (margin_top >= 0 or margin_bottom >= 81):
+		margin_top = 0; margin_bottom = 81
+		slide_v = 0
+		set_physics_process(false)
 
 func _ready():
-	hide()
-	$Icons/MainIcon.enable()
-
-var dir = 0
-func _physics_process(delta):
-	var port = get_viewport()
-	if port.get_visible_rect().size.y - port.get_mouse_position().y <= 81:
-		timer += delta
-	else:
-		timer = 0; dir = 1000;
-	if timer >= 1:
-		timer = 1; dir = -1000
-	if dir == 0:
-		return
-	if dir < 0 and $Icons.position.y <= 0:
-		dir = 0; return
-	if dir > 0 and $Icons.position.y >= 81:
-		dir = 0; return
-	$Icons.position.y += dir * delta
+	set_physics_process(false)
+	$UpTimer.connect("timeout", self, "slide", [-1])
+	$DownTimer.connect("timeout", self, "slide", [1])
