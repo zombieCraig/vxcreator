df5f575246b621829f58673550e189ba09ba5d46
lookatthesea <shane.atkinson@lookatthesea.com>
2018-04-27 15:23:44 -0400

    Added window positions. Added fixed visible artifiacts.

--- Src/Scenes/Menu/Desktop.gd
+++ Src/Scenes/Menu/Desktop.gd
@@ -3,7 +3,8 @@ extends Control
 func _ready():
 	$MenuPosition.set_anchors_preset(Control.PRESET_CENTER)
 	$TaskBar.no_attention($TaskBar/MainIcon)
-	$TaskBar/MainIcon.connect("button_up", self, "icon_click", [$MenuPosition/MenuBox])
+	$TaskBar/MainIcon.connect("button_up", self, "icon_click", [$MenuPosition, $MenuPosition/MenuBox])
 
-func icon_click(item):
+func icon_click(box, item):
+	box.visible = !box.visible
 	item.visible = !item.visible
